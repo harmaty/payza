abd988454d157ff2082d6d5428b01f104b2ebb5e
Artem Harmaty <harmaty@gmail.com>
2014-08-29 17:07:58 +0400

    require httparty

--- lib/payza.rb
+++ lib/payza.rb
@@ -1,29 +1,28 @@
-module Payza
+require 'httparty'
 
-  class Payza
+class Payza
 
-    SANDBOX_URL = 'https://sandbox.payza.com/api/api.svc/'
-    API_URL = 'https://api.payza.com/svc/api.svc/'
+  SANDBOX_URL = 'https://sandbox.payza.com/api/api.svc/'
+  API_URL = 'https://api.payza.com/svc/api.svc/'
 
-    def initialize(account, api_secret, sandbox = false)
-      @url = sandbox ? SANDBOX_URL : API_URL
-      @account = account
-      @api_secret = api_secret
-    end
-
-    def api_call(data, method)
-      data.merge!({"USER" => @account, "PASSWORD" => @api_secret})
-      query = {}
-      data.each do |key, value|
-        query[key.to_s.upcase] = URI.escape(value.to_s)
-      end
-
-      parse_response HTTParty.post(@url + method, body: query)
-    end
-
-    def parse_response(input)
-      @response_array = Rack::Utils.parse_nested_query(input)
-    end
+  def initialize(account, api_secret, sandbox = false)
+    @url = sandbox ? SANDBOX_URL : API_URL
+    @account = account
+    @api_secret = api_secret
   end
 
+  def api_call(data, method)
+    data.merge!({"USER" => @account, "PASSWORD" => @api_secret})
+    query = {}
+    data.each do |key, value|
+      query[key.to_s.upcase] = URI.escape(value.to_s)
+    end
+
+    parse_response HTTParty.post(@url + method, body: query)
+  end
+
+  def parse_response(input)
+    @response_array = Rack::Utils.parse_nested_query(input)
+  end
 end
+
